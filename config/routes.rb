59925d86b14ea2099906f298bb4c9da8610f734f
github username <github email address>
2018-04-16 12:14:18 -0400

    adding user pledge project and category models and view files for CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :projects
+  resources :pledges
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
