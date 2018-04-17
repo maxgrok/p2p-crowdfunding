fa70c8df39c0f9c8e70f1cffad63600779145c47
github username <github email address>
2018-04-17 10:00:09 -0400

    adding edits to views and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+	validates :name, presence: true
+	validates :age, presence: true
+	
 	has_many :projects
 	has_many :pledges, :foreign_key => "donor_id" 
 	has_many :projects, :foreign_key => "beneficiary_id"
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users
   resources :category
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'home#index'
+  get '/about', to: "home#about"
 end
